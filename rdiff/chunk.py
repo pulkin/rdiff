538b719fe475d13ea25505193e9003ef79aeff52
Artem Pulkin <gpulkin@gmail.com>
2024-09-02 00:25:59 +0200

    chunk: add to_string

--- rdiff/chunk.py
+++ rdiff/chunk.py
@@ -2,6 +2,7 @@ from typing import Any, NamedTuple, Optional, Union
 from collections.abc import Sequence
 from functools import reduce
 from operator import add
+from itertools import chain
 
 
 class Chunk(NamedTuple):
@@ -23,7 +24,29 @@ class Chunk(NamedTuple):
     """
     data_a: Sequence[Any]
     data_b: Sequence[Any]
-    eq: Union[bool, Sequence["Diff"]]
+    eq: Union[bool, Sequence[Union[bool, "Diff"]]]
+
+    def to_string(self, prefix: str = "") -> str:
+        eq = self.eq
+        data_a = self.data_a
+        data_b = self.data_b
+
+        if eq is True:
+            if data_a == data_b:
+                return f"{prefix}a=b: {repr(data_a)}"
+            else:
+                return f"{prefix}a=b: {repr(data_a)} = {repr(data_b)}"
+        elif eq is False:
+            return f"{prefix}a≠b: {repr(data_a)} ≠ {repr(data_b)}"
+        else:
+            result = [f"{prefix}a≈b:"]
+            for _eq, _a, _b in zip(eq, data_a, data_b):
+                if _eq is True:
+                    # this was an exact comparison
+                    result.append(f"{prefix}··a[]=b[] {_a}")
+                else:
+                    result.append(_eq.to_string(prefix=prefix + "··"))
+            return "\n".join(result)
 
 
 class Diff(NamedTuple):
@@ -79,3 +102,10 @@ class Diff(NamedTuple):
         """
         assert self.diffs is not None
         return reduce(add, (i.data_b for i in self.diffs))
+
+    def to_string(self, prefix: str = "") -> str:
+        preamble = f"{prefix}Diff({self.ratio:.4f})"
+        if self.diffs is None:
+            return preamble
+        else:
+            return "\n".join(chain((preamble + ":",), (i.to_string(prefix=prefix + "··") for i in self.diffs)))
