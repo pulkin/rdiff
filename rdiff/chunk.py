965b674d0b49a27569b725d17a544ee1ae7a9e68
Artem Pulkin <gpulkin@gmail.com>
2024-11-02 13:34:16 +0100

    chunk: iter_important

--- rdiff/chunk.py
+++ rdiff/chunk.py
@@ -1,5 +1,5 @@
 from typing import Any, Optional, Union
-from collections.abc import Sequence
+from collections.abc import Sequence, Iterator
 from functools import reduce, cached_property
 from operator import add
 from itertools import chain
@@ -161,3 +161,94 @@ class Diff:
         if self.diffs is None:
             raise ValueError("no diff data")
         return Signature(parts=tuple(i.signature for i in self.diffs))
+
+    def iter_important(self, context_size: int = 0) -> Iterator[Union["Item", int]]:
+        """
+        Iterates over non-equal item pairs.
+
+        Parameters
+        ----------
+        context_size
+            The number of equal pairs to provide the context
+            while yielding non-equal pairs.
+
+        Yields
+        ------
+        Diff items and integers specifying the number of skipped
+        item pairs in-between.
+        """
+        def _dummy():
+            return
+            yield
+
+        def _head(data_a, counter_a, data_b, counter_b):
+            for (i, a), (j, b) in zip(
+                enumerate(data_a[:context_size], counter_a),
+                enumerate(data_b[:context_size], counter_b),
+            ):
+                yield Item(a=a, b=b, ix_a=i, ix_b=j)
+
+        def _tail(data_a, counter_a, data_b, counter_b, head_size):
+            gap = len(data_a) - context_size - head_size
+            if gap > 0:
+                yield gap
+            else:
+                gap = 0
+            gap += head_size
+            for (i, a), (j, b) in zip(
+                enumerate(data_a[gap:], counter_a + gap),
+                enumerate(data_b[gap:], counter_b + gap),
+            ):
+                yield Item(a=a, b=b, ix_a=i, ix_b=j)
+
+        context_tail = _dummy()
+        counter_a = counter_b = 0
+
+        for i_chunk, chunk in enumerate(self.diffs):
+
+            if chunk.eq is True:  # chunks are equal: take care of context
+                if i_chunk:  # this is NOT the beginning of text: yield context
+                    yield from _head(chunk.data_a, counter_a, chunk.data_b, counter_b)
+                context_tail = _tail(chunk.data_a, counter_a, chunk.data_b, counter_b, bool(i_chunk) * context_size)
+
+            elif chunk.eq is False:  # chunks are not equal: yield them all
+                yield from context_tail
+                for i, a in enumerate(chunk.data_a, counter_a):
+                    yield Item(a=a, b=None, ix_a=i, ix_b=None)
+                for j, b in enumerate(chunk.data_b, counter_b):
+                    yield Item(a=None, b=b, ix_a=None, ix_b=j)
+
+            else:  # chunks are aligned
+                yield from context_tail
+                for i, (a, b, diff) in enumerate(zip(chunk.data_a, chunk.data_b, chunk.eq)):
+                    yield Item(a=a, b=b, ix_a=counter_a + i, ix_b=counter_b + i, diff=diff)
+
+            counter_a += len(chunk.data_a)
+            counter_b += len(chunk.data_b)
+
+        leftover = sum(i if isinstance(i, int) else 1 for i in context_tail)
+        if leftover:
+            yield leftover
+
+
+@dataclass(frozen=True)
+class Item:
+    """
+    Represents a diff item.
+
+    Parameters
+    ----------
+    a
+    b
+        The two objects in diff.
+    ix_a
+    ix_b
+        The corresponding sequence indexes.
+    diff
+        An optional diff between a and b.
+    """
+    a: Any
+    b: Any
+    ix_a: Optional[int]
+    ix_b: Optional[int]
+    diff: Optional[Diff] = None
--- tests/test_chunk.py
+++ tests/test_chunk.py
@@ -1,4 +1,4 @@
-from rdiff.chunk import Diff, Chunk
+from rdiff.chunk import Diff, Chunk, Item
 
 
 def test_diff():
@@ -11,3 +11,38 @@ def test_diff():
     )
     assert diff.get_a() == "helloworld"
     assert diff.get_b() == "hiworld"
+
+
+def test_important():
+    diff_345 = Diff(ratio=2 / 3, diffs=[
+        Chunk(data_a=[3], data_b=[3], eq=True),
+        Chunk(data_a=[4], data_b=[9], eq=False),
+        Chunk(data_a=[5], data_b=[5], eq=True),
+    ])
+    diff = Diff(
+        ratio=5. / 6,
+        diffs=[
+            Chunk(data_a=[[0, 1, 2]], data_b=[[0, 1, 2]], eq=True),
+            Chunk(data_a=[[3, 4, 5]], data_b=[[3, 9, 5]], eq=[diff_345]),
+            Chunk(data_a=[[6, 7, 8]], data_b=[[6, 7, 8]], eq=True),
+            Chunk(data_a=[[9, 10, 11]], data_b=[[19, 20, 21]], eq=False),
+            Chunk(data_a=[[12, 13, 14], [15, 16, 17]], data_b=[[12, 13, 14], [15, 16, 17]], eq=True),
+        ]
+    )
+    assert list(diff.iter_important()) == [
+        1,
+        Item(a=[3, 4, 5], b=[3, 9, 5], ix_a=1, ix_b=1, diff=diff_345),
+        1,
+        Item(a=[9, 10, 11], b=None, ix_a=3, ix_b=None),
+        Item(a=None, b=[19, 20, 21], ix_a=None, ix_b=3),
+        2,
+    ]
+    assert list(diff.iter_important(context_size=1)) == [
+        Item(a=[0, 1, 2], b=[0, 1, 2], ix_a=0, ix_b=0),
+        Item(a=[3, 4, 5], b=[3, 9, 5], ix_a=1, ix_b=1, diff=diff_345),
+        Item(a=[6, 7, 8], b=[6, 7, 8], ix_a=2, ix_b=2),
+        Item(a=[9, 10, 11], b=None, ix_a=3, ix_b=None),
+        Item(a=None, b=[19, 20, 21], ix_a=None, ix_b=3),
+        Item(a=[12, 13, 14], b=[12, 13, 14], ix_a=4, ix_b=4),
+        1,
+    ]
