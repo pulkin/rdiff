17e7651edf647ffb2fd245c908336a5a9cff48a6
Artem Pulkin <gpulkin@gmail.com>
2024-12-01 22:37:03 +0100

    path_util: initial implementation of path walk

---
+++ rdiff/cli/path_util.py
@@ -0,0 +1,220 @@
+from pathlib import Path
+import logging
+from typing import Optional
+from collections.abc import Iterator, Sequence, Callable
+import re
+import fnmatch
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class MatchRule:
+    accept: bool
+    """
+    A rule matching paths.
+    
+    Parameters
+    ----------
+    accept
+        A flag indicating whether matching paths have to be accepted or declined.
+    """
+
+    def match(self, key: str) -> bool:
+        """
+        Attempts to match a path through the provided key.
+
+        Parameters
+        ----------
+        key
+            The key to match.
+
+        Returns
+        -------
+        True if the key matches. This implementation always returns True.
+        """
+        return True
+
+    def __str__(self):
+        if self.accept:
+            return "rule: include all"
+        else:
+            return "rule: exclude all"
+
+
+accept_all = MatchRule(True)
+reject_all = MatchRule(False)
+
+
+@dataclass(frozen=True)
+class RegexMatchRule(MatchRule):
+    pattern: str
+    pattern_str: str = None
+    """
+    A rule matching paths.
+    
+    Parameters
+    ----------
+    accept
+        A flag indicating whether matching paths have to be accepted or declined.
+    pattern
+        Regex pattern to match.
+    pattern_str
+        The representation of the regex pattern.
+    """
+    def __post_init__(self):
+        object.__setattr__(self, "pattern_str", self.pattern_str or self.pattern)
+
+    @classmethod
+    def from_glob(cls, accept: bool, glob_pattern: str):
+        """
+        Defines a regex pattern rule from the provided glob.
+
+        Parameters
+        ----------
+        accept
+            A flag indicating whether matching paths have to be accepted or declined.
+        glob_pattern
+            Glob pattern to match.
+
+        Returns
+        -------
+        The match rule.
+        """
+        return RegexMatchRule(accept, fnmatch.translate(glob_pattern), glob_pattern)
+
+    def match(self, key: str) -> bool:
+        """
+        Attempts to match a path through the provided key.
+
+        Parameters
+        ----------
+        key
+            The key to match.
+
+        Returns
+        -------
+        True if the key matches.
+        """
+        return bool(re.fullmatch(self.pattern, key))
+
+    def __str__(self):
+        prefix = "include" if self.accept else "exclude"
+        return f"{prefix} {self.pattern_str!r}"
+
+
+glob_rule = RegexMatchRule.from_glob
+accept_folders = glob_rule(True, "*/")
+
+
+def iterdir(
+        node: Path,
+        root: Optional[Path] = None,
+        rules: Sequence[MatchRule] = tuple(),
+) -> Iterator[tuple[Path, MatchRule, str]]:
+    """
+    Recursive iteration over path tree using rsymc-like rules.
+
+    Parameters
+    ----------
+    node
+        The node to look into. Accepts folders or files or
+        non-existent nodes but will yield only the existing and
+        matching ones: whether they are files or folders.
+
+        Symlinks are ignored.
+    root
+        The root which is used to construct keys for matching
+        paths.
+    rules
+        The rules to use when iterating the path tree.
+
+    Yields
+    ------
+    path
+        A matching path.
+    rule
+        The rule mathing the path.
+    key
+        A key that matches the rule.
+    """
+    if not node.exists():
+        return
+    if node.is_symlink():
+        logging.warning("ignoring symlink: %s", node)
+        return
+    if root is None:
+        root = node
+    key = str(node.relative_to(root))
+    if node.is_dir():
+        key += "/"
+    for rule in rules:
+        if rule.match(key):
+            if rule.accept:
+                yield node, rule, key
+                if node.is_dir():
+                    for sub_node in node.iterdir():
+                        yield from iterdir(sub_node, root, rules)
+                break
+            else:  # reject explicitly
+                break
+
+
+def iter_match(
+        a: Path,
+        b: Path,
+        transform: Optional[Callable[(str,), str]] = None,
+        rules: Sequence[MatchRule] = tuple(),
+) -> Iterator[tuple[Optional[Path], Optional[Path], str]]:
+    """
+    Iterates two path trees and matches the nodes.
+
+    Parameters
+    ----------
+    a
+    b
+        The two paths to traverse.
+    transform
+        An optional transform for path keys.
+    rules
+        The rules to use when iterating the path trees.
+
+    Yields
+    ------
+    path_a
+    path_b
+        Two matching paths. If one of the paths does not
+        match the other one is set to None.
+    key
+        A key for both paths.
+    """
+    def _collect(_path: Path, _name: str) -> dict[str, Path]:
+        _result = {}
+        logging.info("collecting %s ...", _name)
+        for _child, _rule, _key in iterdir(
+                node=_path,
+                root=_path,
+                rules=rules,
+        ):
+            if _child.is_file():
+                logging.debug("matched %s in %s: %s", _child, _name, _rule)
+                if transform is not None:
+                    _key = transform(_key)
+                if _key in _result:
+                    logging.warning("multiple paths transform into the same key %s", _key)
+                    logging.warning("  path: %s", _child)
+                _result[_key] = _child
+        return _result
+
+    a_contents = _collect(a, "a")
+    b_contents = _collect(b, "b")
+
+    for key, a_path in a_contents.items():
+        try:
+            b_path = b_contents.pop(key)
+        except KeyError:
+            yield a_path, None, key
+        else:
+            yield a_path, b_path, key
+
+    for key, b_path in b_contents.items():
+        yield None, b_path, key
---
+++ tests/test_path_util.py
@@ -0,0 +1,143 @@
+from pathlib import Path
+from collections.abc import Iterator
+from rdiff.cli.path_util import iterdir, accept_all, accept_folders, reject_all, glob_rule
+
+
+def test_no_files(tmp_path):
+    assert set(iterdir(tmp_path, rules=[reject_all])) == set()
+
+
+def test_no_files_r(tmp_path):
+    assert set(iterdir(tmp_path, rules=[accept_all])) == {
+        (tmp_path, accept_all, "./"),
+    }
+
+
+def test_simple(tmp_path):
+    (tmp_path / "1.txt").touch()
+    (tmp_path / "data").touch()
+
+    assert set(iterdir(tmp_path, rules=[reject_all])) == set()
+
+
+def test_simple_r(tmp_path):
+    (tmp_path / "1.txt").touch()
+    (tmp_path / "data").touch()
+
+    assert set(iterdir(tmp_path, rules=[accept_all])) == {
+        (tmp_path, accept_all, "./"),
+        (tmp_path / "1.txt", accept_all, "1.txt"),
+        (tmp_path / "data", accept_all, "data"),
+    }
+
+
+def test_simple_depth_r(tmp_path):
+    (tmp_path / "1.txt").touch()
+    (tmp_path / "data").mkdir()
+    (tmp_path / "data" / "2.txt").touch()
+
+    assert set(iterdir(tmp_path, rules=[accept_all])) == {
+        (tmp_path, accept_all, "./"),
+        (tmp_path / "1.txt", accept_all, "1.txt"),
+        (tmp_path / "data", accept_all, "data/"),
+        (tmp_path / "data/2.txt", accept_all, "data/2.txt"),
+    }
+
+
+def test_simple_exclude(tmp_path):
+    (tmp_path / "1.txt").touch()
+    (tmp_path / "data").touch()
+
+    rules = [glob_rule(False, "1.txt"), accept_all]
+
+    assert set(iterdir(tmp_path, rules=rules)) == {
+        (tmp_path, accept_all, "./"),
+        (tmp_path / "data", accept_all, "data"),
+    }
+
+
+def test_simple_include(tmp_path):
+    (tmp_path / "1.txt").touch()
+    (tmp_path / "data").touch()
+
+    rules = [glob_rule(True, "1.txt"), accept_folders, reject_all]
+
+    assert set(iterdir(tmp_path, rules=rules)) == {
+        (tmp_path, accept_folders, "./"),
+        (tmp_path / "1.txt", rules[0], "1.txt"),
+    }
+
+def test_nested(tmp_path):
+    (tmp_path / "1.txt").touch()
+    (tmp_path / "data").mkdir()
+    (tmp_path / "data" / "1.txt").touch()
+
+    rules = [
+        glob_rule(True, "1.txt"),
+        glob_rule(True, "data/"),
+        glob_rule(True, "./"),
+    ]
+
+    assert set(iterdir(tmp_path, rules=rules)) == {
+        (tmp_path, rules[2], "./"),
+        (tmp_path / "1.txt", rules[0], "1.txt"),
+        (tmp_path / "data", rules[1], "data/"),
+    }
+
+
+def test_nested_all(tmp_path):
+    (tmp_path / "1.txt").touch()
+    (tmp_path / "data").mkdir()
+    (tmp_path / "data" / "1.txt").touch()
+
+    rules = [
+        glob_rule(True, "*1.txt"),
+        accept_folders,
+    ]
+
+    assert set(iterdir(tmp_path, rules=rules)) == {
+        (tmp_path, accept_folders, "./"),
+        (tmp_path / "1.txt", rules[0], "1.txt"),
+        (tmp_path / "data", accept_folders, "data/"),
+        (tmp_path / "data/1.txt", rules[0], "data/1.txt"),
+    }
+
+
+def test_nested_some(tmp_path):
+    (tmp_path / "1.txt").touch()
+    (tmp_path / "data").mkdir()
+    (tmp_path / "data" / "1.txt").touch()
+
+    rules = [
+        glob_rule(True, "*1.txt"),
+        glob_rule(True, "./"),
+    ]
+
+    assert set(iterdir(tmp_path, rules=rules)) == {
+        (tmp_path, rules[1], "./"),
+        (tmp_path / "1.txt", rules[0], "1.txt"),
+    }
+
+
+def test_nested_complex(tmp_path):
+    (tmp_path / "data").mkdir()
+    (tmp_path / "data/important").mkdir()
+    (tmp_path / "data/important/file.txt").touch()
+    (tmp_path / "data/unimportant.txt").touch()
+    (tmp_path / "data/stuff").mkdir()
+    (tmp_path / "data/stuff/something.txt").touch()
+
+    rules = [
+        glob_rule(True, "./"),
+        glob_rule(True, "data/"),
+        glob_rule(True, "data/important/"),
+        glob_rule(True, "data/important/*"),
+        glob_rule(True, "data/stuff/something.txt"),
+    ]
+
+    assert set(iterdir(tmp_path, rules=rules)) == {
+        (tmp_path, rules[0], "./"),
+        (tmp_path / "data", rules[1], "data/"),
+        (tmp_path / "data/important", rules[2], "data/important/"),
+        (tmp_path / "data/important/file.txt", rules[3], "data/important/file.txt"),
+    }
