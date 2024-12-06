311f1d80578efc9a31de148b4dad923c0e24b7dd
Artem Pulkin <gpulkin@gmail.com>
2024-12-06 23:43:04 +0100

    tests: fix updated test

--- tests/util.py
+++ tests/util.py
@@ -8,14 +8,4 @@ def np_chunk_eq(a: Chunk, b: Chunk) -> bool:
 
 
 def np_chunk_eq_aligned(a: Chunk, b: Chunk) -> bool:
-    def np2tuple(i):
-        if not isinstance(i, (list, tuple)):
-            return i
-        return type(i)([
-            j.tolist()
-            if isinstance(j, np.ndarray)
-            else j
-            for j in i
-        ])
-
-    return (a.data_a == b.data_a).all() and (a.data_b == b.data_b).all() and np2tuple(a.eq) == np2tuple(b.eq)
+     return (a.data_a == b.data_a).all() and (a.data_b == b.data_b).all() and (str(a.eq) == str(b.eq))
