0e75600a47855f823a5a57767cdd3170873a6d3c
Artem Pulkin <gpulkin@gmail.com>
2025-01-07 21:28:51 +0100

    tests: refactor

--- tests/test_presentation/test_base.py
+++ tests/test_presentation/test_base.py
@@ -1,13 +1,11 @@
 from pathlib import Path
-from subprocess import Popen, check_output
-import tarfile
 
 import pytest
 import pandas as pd
 
 from rdiff.presentation.base import MarkdownTableFormats
 
-from .util import diff2text
+from .util import diff2text, self_extract, sync_contents
 
 cases = Path(__file__).parent / "cases"
 
@@ -35,44 +33,16 @@ def test_co2(tmp_path, test_diff_renders, fmt, out_fmt):
     if out_fmt == "md":
         p_kwargs["table_formats"] = MarkdownTableFormats()
     text = diff2text(base / f"a.{fmt}", base / f"b.{fmt}", min_ratio=0, min_ratio_row=0, printer_kwargs=p_kwargs)
-
-    with open(cases / f"co2_emissions/diff.{fmt}.{out_fmt}", "r" if test_diff_renders else "w") as f:
-        if test_diff_renders:
-            assert text == f.read()
-        else:
-            f.write(text)
+    sync_contents(cases / f"co2_emissions/diff.{fmt}.{out_fmt}", text, test_diff_renders)
 
 
 def test_readme(test_diff_renders):
     text = diff2text(cases / "readme/a.txt", cases / "readme/b.txt")
-    if test_diff_renders:
-        with open(cases / "readme/diff.txt", "r") as f:
-            assert text == f.read()
-    else:
-        with open(cases / "readme/diff.txt", "w") as f:
-            f.write(text)
+    sync_contents(cases / "readme/diff.txt", text, test_diff_renders)
 
 
 def test_git_history(tmp_path, test_diff_renders):
-    a = tmp_path / "a"
-    b = tmp_path / "b"
-    root = check_output(["git", "rev-parse", "--show-toplevel"], text=True).strip()
-    for src, dst in [
-        ("0277db1191fa0189699ecf941664bdeab292f7bb", a),
-        ("4cedf58add8512ea1ed13e3c38e7faf86ba227d6", b),
-    ]:
-        t = dst.parent / f"{dst.name}.tar"
-        with open(t, "w+b") as f:
-            Popen(["git", "-C", root, "archive", src], stdout=f).communicate()
-            f.flush()
-            f.seek(0)
-            with tarfile.open(fileobj=f) as f_tar:
-                f_tar.extractall(dst)
-
+    self_extract("0277db1191fa0189699ecf941664bdeab292f7bb", a := tmp_path / "a")
+    self_extract("4cedf58add8512ea1ed13e3c38e7faf86ba227d6", b := tmp_path / "b")
     text = diff2text(a, b)
-    if test_diff_renders:
-        with open(cases / "git/diff.txt", "r") as f:
-            assert text == f.read()
-    else:
-        with open(cases / "git/diff.txt", "w") as f:
-            f.write(text)
+    sync_contents(cases / "git/diff.txt", text, test_diff_renders)
--- tests/test_presentation/util.py
+++ tests/test_presentation/util.py
@@ -1,4 +1,7 @@
 from io import StringIO
+from subprocess import Popen, check_output
+import tarfile
+from tempfile import NamedTemporaryFile
 
 from rdiff.cli.processor import process_iter
 from rdiff.presentation.base import TextPrinter
@@ -14,3 +17,21 @@ def diff2text(a, b, printer_kwargs=None, **kwargs):
         printer.print_diff(i)
 
     return buffer.getvalue()
+
+
+def self_extract(commit, dst):
+    root = check_output(["git", "rev-parse", "--show-toplevel"], text=True).strip()
+    with NamedTemporaryFile("w+b") as f:
+        Popen(["git", "-C", root, "archive", commit], stdout=f).communicate()
+        f.flush()
+        f.seek(0)
+        with tarfile.open(fileobj=f) as f_tar:
+            f_tar.extractall(dst)
+
+
+def sync_contents(path, content, check):
+    with open(path, "r" if check else "w") as f:
+        if check:
+            assert content == f.read()
+        else:
+            f.write(content)
