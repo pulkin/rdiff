4e6a7df04d0e3eac654113571643f7a6038bd53d
Artem Pulkin <gpulkin@gmail.com>
2025-06-09 23:30:53 +0200

    setup.py: simplify

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, Extension, find_namespace_packages
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 extensions = [
@@ -24,7 +24,7 @@ setup(
         include_path=["sdiff/cython"],
         annotate=True,
     ),
-    packages=find_namespace_packages(),
+    packages=["sdiff"],
     zip_safe=False,
     include_package_data=True,
 )
